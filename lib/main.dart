0db9551245e72d1b2d495d6b27a58f6216253c04
Himanshuranjan30 <himanshuranjan30@gmail.com>
2020-10-11 00:54:27 +0530

    added

---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: Request(),
+    );
+  }
+}
+
+class Request extends StatefulWidget {
+  @override
+  _RequestState createState() => _RequestState();
+}
+
+class _RequestState extends State<Request> {
+  Map data = {'key1': 1, 'key2': "some text"};
+
+  senddata() async {
+    String body = json.encode(data);
+    http.Response response = await http.post(
+      'http://localhost:5001',
+      headers: {"Content-Type": "application/json"},
+      body: body,
+    );
+    print(response.body);
+  }
+
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: FlatButton(
+          onPressed: () => senddata(),
+          child: Text('Post'),
+        ),
+      ),
+    );
+  }
+}
